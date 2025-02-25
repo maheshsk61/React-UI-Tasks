d3b830e3b7352ac963c6686db469ce9f409e2300
maheshsk61 <maheshsk61@gmail.com>
2025-02-26 00:26:53 +0530

    completed dynamic router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,33 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useParams,
+} from "react-router-dom";
 import ToDoList from "./TasksList/to-do-list.jsx";
+import Pagination from "./TasksList/pagination.jsx";
 
-const routers = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "/tasks/:taskName",
-      element: <ToDoList />,
-    },
-  ]
-);
+const DynamicTask = () => {
+  const { taskName } = useParams();
+  return taskName === "toDo" ? (
+    <ToDoList />
+  ) : taskName === "pagination" ? (
+    <Pagination />
+  ) : null;
+};
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/tasks/:taskName",
+    element: <DynamicTask />,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={routers} />
 );
